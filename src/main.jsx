320d0f75e2f2a13f683a9983f7e9b7ed7eee4b6d
Pradip Bedre <bedrepradip0@gmail.com>
2023-01-22 10:22:07 +0530

    all done

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,18 @@ import { BrowserRouter } from "react-router-dom";
 import store from "./Store";
 import { Provider } from "react-redux";
 import { ConfigProvider } from "antd";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://38ce8258b2504deeb45f03295c2a2a4f@o4504546279620608.ingest.sentry.io/4504546289451008",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
